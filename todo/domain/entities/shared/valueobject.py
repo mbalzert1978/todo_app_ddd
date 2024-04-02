9e5abe945e994751430cdda3cfb5675bec941d69
Markus Iorio <mbalzert@gmail.com>
2024-04-02 22:53:56 +0200

    base entity and value objects

---
+++ todo/domain/entities/shared/valueobject.py
@@ -0,0 +1,15 @@
+import dataclasses
+import typing
+
+
+@dataclasses.dataclass(frozen=True)
+class ValueObject:
+    value: typing.Any
+
+    def __eq__(self, __value: object) -> bool:
+        return isinstance(__value, type(self)) and self.value == __value
+
+    def __hash__(self) -> int:
+        if isinstance(self.value, typing.Iterable):
+            return hash(hash(x) for x in self.value)
+        return hash(self.value)
