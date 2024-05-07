d03a98f88adea5ae2c14d2dd32727d6c7f1a1f5e
Markus Iorio <markus.iorio@bfd.de>
2024-05-07 14:03:30 +0200

    entity abstract and

--- todo/shared_kernel/entity.py
+++ todo/shared_kernel/entity.py
@@ -1,16 +1,15 @@
+import abc
 import dataclasses
 
-from todo.shared_kernel.uuid_id import UuidId
 from todo.shared_kernel.valueobject import ValueObject
 
-DEFAULT_FN = UuidId.generate
 
 @dataclasses.dataclass
-class Entity:
-    id: ValueObject = dataclasses.field(default_factory=DEFAULT_FN)
+class Entity(abc.ABC):
+    id: ValueObject
 
     def __eq__(self, __value: object) -> bool:
         return isinstance(__value, type(self)) and self.id == __value.id
 
     def __hash__(self) -> int:
-        return hash(self)
+        return hash(self.id)
