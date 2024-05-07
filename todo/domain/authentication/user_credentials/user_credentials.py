bfda61e6cae01c743c8ddb40bf5a56d02c3c7cb4
Markus Iorio <markus.iorio@bfd.de>
2024-05-07 10:39:28 +0200

    new nf

--- todo/domain/authentication/user_credentials/user_credentials.py
+++ todo/domain/authentication/user_credentials/user_credentials.py
@@ -1,20 +1,28 @@
 import dataclasses
+import typing
 
 from todo.domain.authentication.abstraction.hashing import HashingProvider
-from todo.domain.authentication.user_credentials.email import Email
 from todo.domain.authentication.user_credentials.password import Password
 from todo.domain.authentication.user_credentials.password_hash import (
     PasswordHash,
 )
 from todo.shared_kernel.entity import Entity
 from todo.shared_kernel.uuid_id import UuidId
+from todo.shared_kernel.valueobject import ValueObject
+
+T = typing.TypeVar("T")
 
 
-@dataclasses.dataclass
-class UserCredentials(Entity):
-    user_id: UuidId
-    email: Email
+@dataclasses.dataclass(kw_only=True)
+class UserCredentials(Entity, typing.Generic[T]):
+    user_id: ValueObject[T]
     password_hash: PasswordHash
+
+    @classmethod
+    def new(
+        cls, user_id: ValueObject[T], password_hash: PasswordHash
+    ) -> "UserCredentials":
+        return cls(id=UuidId.generate(), user_id=user_id, password_hash=password_hash)
 
     def set_password(
         self,
