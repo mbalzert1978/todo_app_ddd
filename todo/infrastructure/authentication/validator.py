5070446ac282184b83be023b156701286c26e1cb
Markus Iorio <markus.iorio@bfd.de>
2024-05-07 14:03:14 +0200

    validator

---
+++ todo/infrastructure/authentication/validator.py
@@ -0,0 +1,50 @@
+import string
+import pydantic
+
+from todo.domain.authentication.abstraction.validation import (
+    EmailValidationProvider,
+    PasswordValidationProvider,
+)
+
+
+class PydanticEmailValidator:
+    """Email Validation Provider Interface."""
+
+    def is_valid_email(self, email: str) -> bool:
+        """Validate Email."""
+
+        class _Email(pydantic.BaseModel):
+            email: pydantic.EmailStr
+
+        try:
+            _Email(email=email)
+        except pydantic.ValidationError:
+            return False
+        else:
+            return True
+
+
+class PasswordValidator:
+    """Password Validation Provider Interface."""
+
+    def is_valid_password(self, password: str) -> bool:
+        """Validate Password."""
+        if (
+            len(password) < 8
+            or not any(char.isdigit() for char in password)
+            or not any(char in string.punctuation for char in password)
+        ):
+            return False
+        return True
+
+
+class PydanticValidator:
+    @property
+    def email(self) -> EmailValidationProvider:
+        """Email Validation Provider."""
+        return PydanticEmailValidator()
+
+    @property
+    def password(self) -> PasswordValidationProvider:
+        """Password Validation Provider."""
+        return PasswordValidator()
