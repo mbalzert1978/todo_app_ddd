0c5452c81648483f66503ff0202ee601ffe2f47a
Markus Iorio <markus.iorio@bfd.de>
2024-05-07 13:57:18 +0200

    signup usecase

--- todo/application/use_cases/signup_usecase/signup_service.py
+++ todo/application/use_cases/signup_usecase/signup_service.py
@@ -1,15 +1,12 @@
 from todo.application.use_cases.exceptions import EmailInUseError
 from todo.domain.authentication.abstraction.hashing import HashingProvider
-from todo.domain.authentication.abstraction.repository import (
-    UserCredentialRepository,
-    UserRepository,
-)
+from todo.domain.authentication.abstraction.repository import UserRepository
 from todo.domain.authentication.abstraction.validation import ValidationProvider
 from todo.domain.authentication.user.email import Email
+from todo.domain.authentication.user.password import Password
+from todo.domain.authentication.user.password_hash import PasswordHash
 from todo.domain.authentication.user.user import User
 from todo.domain.authentication.user.user_name import UserName
-from todo.domain.authentication.user_credentials.password_hash import PasswordHash
-from todo.domain.authentication.user_credentials.user_credentials import UserCredentials
 
 
 class SignUpService:
@@ -18,12 +15,10 @@ class SignUpService:
         hash_provider: HashingProvider,
         validation_provider: ValidationProvider,
         users: UserRepository,
-        credentials: UserCredentialRepository,
     ) -> None:
         self.hasher = hash_provider
         self.validator = validation_provider
         self.users = users
-        self.credentials = credentials
 
     def sign_up(self, name: str, email: str, password: str) -> None:
         if self.users.get_by_email(email) is not None:
@@ -31,16 +26,14 @@ class SignUpService:
 
         user_name = UserName.new(name)
         user_email = Email.new(email, self.validator.email)
-
-        user = User.new(name=user_name, email=user_email)
-
-        salt = self.hasher.generate_salt()
-        password_hash = PasswordHash.new(password, salt, self.hasher)
-
-        user_credentials = UserCredentials.new(
-            user_id=user.id, password_hash=password_hash
+        user_password = Password.new(password, self.validator.password)
+        password_hash = PasswordHash.new(
+            user_password.value,
+            self.hasher.generate_salt(),
+            self.hasher,
         )
 
-        with self.users as users, self.credentials as credentials:
+        user = User.new(name=user_name, email=user_email, password_hash=password_hash)
+
+        with self.users as users:
             users.add(user)
-            credentials.add(user_credentials)
