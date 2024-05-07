99954e3eb9566f95930d699b8dec4f4c4954b0d9
Markus Iorio <markus.iorio@bfd.de>
2024-05-07 10:39:11 +0200

    default id behavior

--- todo/shared_kernel/entity.py
+++ todo/shared_kernel/entity.py
@@ -1,11 +1,13 @@
 import dataclasses
 
+from todo.shared_kernel.uuid_id import UuidId
 from todo.shared_kernel.valueobject import ValueObject
 
+DEFAULT_FN = UuidId.generate
 
 @dataclasses.dataclass
 class Entity:
-    id: ValueObject
+    id: ValueObject = dataclasses.field(default_factory=DEFAULT_FN)
 
     def __eq__(self, __value: object) -> bool:
         return isinstance(__value, type(self)) and self.id == __value.id
