0461cb90114765684095930e090250eecf82fda8
Markus Iorio <markus.iorio@bfd.de>
2024-05-07 14:02:14 +0200

    interface uses bytes now

--- todo/domain/authentication/abstraction/hashing.py
+++ todo/domain/authentication/abstraction/hashing.py
@@ -2,11 +2,11 @@ import typing
 
 
 class HashingProvider(typing.Protocol):
-    def generate_salt(self) -> str:
+    def generate_salt(self) -> bytes:
         """Generate a random salt."""
 
-    def hash(self, password: str, salt: str) -> str:
+    def hash(self, password: str, salt: bytes) -> bytes:
         """Generate a hash for the given password and salt."""
 
-    def verify(self, password: str, salt: str, hash: str) -> bool:
+    def verify(self, password: str, hash: bytes) -> bool:
         """Verify that the given hash matches the given password and salt."""
